95850a94fbc0439cff31c87d7f30924d168d80ea
choiyoungkwon12 <hpwh4457@naver.com>
2023-08-31 21:58:39 +0900

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,14 @@ package org.example;
 
 import io.reactivex.rxjava3.core.Observable;
 
+import java.util.Arrays;
 import java.util.concurrent.Flow;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Observable<TempInfo> observable = getTemperatures2("NewYork");
+        Observable<TempInfo> observable = getCelsiusTemperatures3("NewYork", "Seoul", "Chicago");
         observable.blockingSubscribe(new TempObserver());
     }
 
@@ -39,5 +41,13 @@ public class Main {
             processor.subscribe(subscriber);
             subscriber.onSubscribe(new TempSubscription(processor, town));
         };
+    }
+
+    private static Observable<TempInfo> getCelsiusTemperatures3(String... towns) {
+        return Observable.merge(Arrays.stream(towns).map(Main::getCelsiusTemperatures2).collect(Collectors.toList()));
+    }
+
+    private static Observable<TempInfo> getCelsiusTemperatures2(String town) {
+        return getTemperatures2(town).map(tempInfo -> new TempInfo(tempInfo.getTown(), (tempInfo.getTemp() - 32) * 5 / 9));
     }
 }
